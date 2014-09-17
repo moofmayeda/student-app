9d6229119dbf6e7a1ec22ef61e38a67298e617bc
Sergii Mertsalov & moof mayeda <student@epicodus.com>
2014-09-17 12:05:21 -0700

    allow users to sign in or out

--- controllers/SignInController.js
+++ controllers/SignInController.js
@@ -1,3 +1,9 @@
 studentRoster.controller('SignInCtrl', function SignInCtrl($scope, StudentsFactory){
   $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
+  $scope.signin = function(student) {
+    var selectedStudent = $scope.students[$scope.students.indexOf(student)]
+    selectedStudent.signedin = selectedStudent.signedin === false ? true: false
+    // selectedStudent.date = new Date();
+  }
 });
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -2,7 +2,7 @@ studentRoster.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
   factory.students = [];
   factory.addStudent = function() {
-    factory.students.push({"name": factory.studentName});
+    factory.students.push({"name": factory.studentName, "signedin" : false });
     factory.studentName = null;
   };
 
@@ -10,5 +10,11 @@ studentRoster.factory('StudentsFactory', function StudentsFactory() {
     var index = factory.students.indexOf(student)
     factory.students.splice(index, 1);
   };
+  factory.signin = function(student) {
+    var selectedStudent = factory.students[factory.students.indexOf(student)]
+    selectedStudent.signedin = selectedStudent.signedin === false ? true: false
+    // selectedStudent.date = new Date();
+  }
+
   return factory;
 });
